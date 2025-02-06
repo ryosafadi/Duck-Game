0dde35f2b70c841c95d64a5a0de44e4db1df9849
Jayc <116532978+JaydenChung@users.noreply.github.com>
2025-02-06 15:50:05 -0800

    add some basic puffer fish animations

--- Assets/Scripts/FishAndForth.cs
+++ Assets/Scripts/FishAndForth.cs
@@ -1,32 +1,56 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using UnityEngine;
 
 public class FishAndForth : MonoBehaviour
 {
-    public GameObject Fish;
-    private Vector3 playerMove;
-    private Vector3 stopA;
-    private Vector3 stopB;
-    private float speed = 1.19f;
-    // Start is called before the first frame update
+    public GameObject puffer; // Reference to the child Puffer GameObject with the Animator
+    private Vector3 stopA;    // First stop position
+    private Vector3 stopB;    // Second stop position
+    private float speed = 1.19f; // Speed of movement
+    private Animator pufferAnimator; // Reference to the Animator component on the child
+    private Vector3 lastPosition; // To track the fish's last position
+
     void Start()
     {
-       playerMove = Fish.transform.position;
-       stopA = new Vector3(0, 0, 0);
-        int move = UnityEngine.Random.Range(4, 10);
-       stopB = new Vector3(move, 0, 0);
-        stopB += playerMove;
-        stopA += playerMove;
+        // Initialize stopA as the fish's starting position
+        stopA = transform.position;
+
+        // Randomize the x-coordinate for stopB
+        float randomOffset = UnityEngine.Random.Range(4, 10);
+        stopB = stopA + new Vector3(randomOffset, 0, 0);
+
+        // Get the Animator component from the child Puffer GameObject
+        if (puffer != null)
+        {
+            pufferAnimator = puffer.GetComponent<Animator>();
+        }
+        else
+        {
+            Debug.LogError("Puffer GameObject is not assigned in the inspector!");
+        }
+
+        // Initialize lastPosition
+        lastPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Inspired from: https://stackoverflow.com/questions/43009515/move-gameobject-back-and-forth
+        // Calculate the interpolation factor using PingPong
         float time = Mathf.PingPong(Time.time * speed, 1);
+
+        // Move the parent Fish GameObject between stopA and stopB
         transform.position = Vector3.Lerp(stopA, stopB, time);
+
+        // Determine the direction of movement
+        Vector3 currentPosition = transform.position;
+        float direction = currentPosition.x - lastPosition.x;
+
+        // Update the Animator parameter on the child Puffer GameObject
+        if (pufferAnimator != null)
+        {
+            pufferAnimator.SetFloat("Direction", direction);
+        }
+
+        // Update lastPosition for the next frame
+        lastPosition = currentPosition;
     }
 }
